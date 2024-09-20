1c49709e8cdc975bd4970f051b31eb8a36f1df67
thanajong <thanajong@outlook.com>
2024-09-20 17:42:40 +0700

    refactor to Vue

--- src/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Sessional installed.");
-  });
-
-  // Optional: If you need to handle any background events (e.g., opening new tabs)
-  // For now, background.js doesn't need much as everything is handled in popup.js and the popup.html page
-  chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({ url: "popup.html" });
-  });
